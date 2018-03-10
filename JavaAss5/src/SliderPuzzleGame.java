86bfab3aaf8748207f581aa00cd32f93353b8ee8
archandatta <35818003+archandatta@users.noreply.github.com>
2018-03-10 16:04:15 -0500

    updatedjavafile

--- JavaAss5/src/SliderPuzzleGame.java
+++ JavaAss5/src/SliderPuzzleGame.java
@@ -27,26 +27,14 @@ public class SliderPuzzleGame extends Application {
 
         Button[][] buttons;
         buttons = new Button[4][4];
-        String[][] buttonImages = {{"Lego_00.png","Lego_01.png", "Lego_03.png","Lego_04.png","Lego_05.png","Lego_06.png",
-                "Lego_07.png","Lego_08.png","Lego_09.png","Lego_10.png","Lego_11.png","Lego_12.png","Lego_13.png",
-                "Lego_14.png","Lego_15.png","Lego_16.png","Lego_17.png","Lego_18.png","Lego_19.png","Lego_20.png",
-                "Lego_21.png","Lego_22.png","Lego_23.png","Lego_24.png","Lego_25.png","Lego_26.png","Lego_27.png",
-                "Lego_28.png","Lego_29.png","Lego_30.png","Lego_31.png","Lego_32.png","Lego_33.png"},{"Numbers.00.png",
-                "Numbers_01.png","Numbers_02.png","Numbers_03.png","Numbers_04.png","Numbers_05.png","Numbers_06.png",
-                "Numbers_07.png","Numbers_08.png","Numbers_09.png","Numbers_10.png","Numbers_11.png","Numbers_12.png",
-                "Numbers_13.png","Numbers_14.png","Numbers_15.png","Numbers_16.png","Numbers_17.png","Numbers_18.png",
-                "Numbers_19.png","Numbers_20.png","Numbers_21.png","Numbers_22.png","Numbers_23.png","Numbers_24.png",
-                "Numbers_25.png","Numbers_26.png","Numbers_27.png","Numbers_28.png","Numbers_29.png","Numbers_30.png",
+        String[][] buttonImages = {{"Lego_00.png","Lego_01.png", "Lego_03.png","Lego_10.png","Lego_11.png","Lego_12.png","Lego_13.png",
+               "Lego_20.png", "Lego_21.png","Lego_22.png","Lego_23.png","Lego_30.png","Lego_31.png","Lego_32.png","Lego_33.png"},{"Numbers.00.png",
+                "Numbers_01.png","Numbers_02.png","Numbers_03.png","Numbers_10.png","Numbers_11.png","Numbers_12.png",
+                "Numbers_13.png","Numbers_20.png","Numbers_21.png","Numbers_22.png","Numbers_23.png","Numbers_30.png",
                 "Numbers_31.png","Numbers_32.png","Numbers_33.png"},{"Pets_00.png","Pets_01.png","Pets_02.png",
-                "Pets_03.png","Pets_04.png","Pets_05.png","Pets_06.png","Pets_07.png","Pets_08.png","Pets_09.png",
-                "Pets_10.png","Pets_11.png","Pets_12.png","Pets_13.png","Pets_14.png","Pets_15.png","Pets_16.png",
-                "Pets_17.png","Pets_18.png","Pets_19.png","Pets_20.png","Pets_21.png","Pets_22.png","Pets_23.png",
-                "Pets_24.png","Pets_25.png","Pets_26.png","Pets_27.png","Pets_28.png","Pets_29.png","Pets_30.png",
+                "Pets_03.png", "Pets_10.png","Pets_11.png","Pets_12.png","Pets_13.png","Pets_20.png","Pets_21.png","Pets_22.png","Pets_23.png", "Pets_30.png",
                 "Pets_31.png","Pets_32.png","Pets_33.png"},{"Scenery_00.png","Scenery_01.png","Scenery_02.png",
-                "Scenery_03.png","Scenery_04.png","Scenery_05.png","Scenery_06.png","Scenery_07.png","Scenery_08.png","Scenery_09.png",
-                "Scenery_10.png","Scenery_11.png","Scenery_12.png","Scenery_13.png","Scenery_14.png","Scenery_15.png","Scenery_16.png",
-                "Scenery_17.png","Scenery_18.png","Scenery_19.png","Scenery_20.png","Scenery_21.png","Scenery_22.png","Scenery_23.png",
-                "Scenery_24.png","Scenery_25.png","Scenery_26.png","Scenery_27.png","Scenery_28.png","Scenery_29.png","Scenery_30.png",
+                "Scenery_03.png", "Scenery_10.png","Scenery_11.png","Scenery_12.png","Scenery_13.png","Scenery_20.png","Scenery_21.png","Scenery_22.png","Scenery_23.png",
                 "Scenery_31.png","Scenery_32.png","Scenery_33.png"}};
         for (int i = 0; i<4; i++) {
             for (int j =0; j<4; j++){
@@ -75,7 +63,7 @@ public class SliderPuzzleGame extends Application {
         puzzlesList.relocate(768, 207);
         puzzlesList.setPrefSize(187, 150);
 
-        puzzlesList.getSelectionModel().select(0);
+        puzzlesList.getSelectionModel().getSelectedItem();
 
         // Thumbnail
         Label thumbNail = new Label();
@@ -101,17 +89,17 @@ public class SliderPuzzleGame extends Application {
                 }
             }
         });
-        if(startStop.getText().equals("Stop"))
-        startStop.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            public void handle(MouseEvent f) {
-                if (f.getButton() == MouseButton.PRIMARY) {
-                    startStop.setText("Start");
-                    startStop.setStyle("-fx-color: DARKGREEN");
-                    thumbNail.setDisable(false);
-                    puzzlesList.setDisable(false);
-                }
-            }
-        });
+//        if(startStop.getText().equals("Stop"))
+//        startStop.setOnMouseClicked(new EventHandler<MouseEvent>() {
+//            public void handle(MouseEvent f) {
+//                if (f.getButton() == MouseButton.PRIMARY) {
+//                    startStop.setText("Start");
+//                    startStop.setStyle("-fx-color: DARKGREEN");
+//                    thumbNail.setDisable(false);
+//                    puzzlesList.setDisable(false);
+//                }
+//            }
+//        });
 //        startStop.setOnMouseClicked(new EventHandler<MouseEvent>() {
 //            public void handle(MouseEvent e) {
 //                if (e.getButton() == MouseButton.PRIMARY) {
